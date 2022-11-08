db4d9858bb9e9830eb736d4f6cc8f4c2cf9dd664
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-11-08 19:18:44 +0100

    commit

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,Pressable, StyleSheet } from 'react-native'
 
-function PrimaryButton({children}) {
+function PrimaryButton({children,onPress}) {
   return (
-    <View>
-        <Text>{children}</Text>
+    <View style={styles.outerContainer}>
+    <Pressable style={({pressed})=>pressed?[ styles.innerContainer,styles.pressed ] : styles.innerContainer} 
+    onPress={onPress} 
+    android_ripple={{color:'#640233'}}>
+        <Text style={styles.buttonText}>{children}</Text>
+    </Pressable>
     </View>
   )
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+    outerContainer:{
+        borderRadius:28,
+        margin:4,
+        overflow: 'hidden',
+        elevation:2,
+
+    },
+   innerContainer:{
+    backgroundColor:"#4e0329",
+    paddingVertical:8,
+    paddingHorizontal:16, 
+   },
+   buttonText:{
+    color:'white',
+    textAlign:'center',
+   },
+   pressed:{
+    opacity:0.75,
+   }
+
+
+
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { TextInput, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
 
 function StartGameScreen() {
   return (
-      <View>
-        <TextInput />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.numberInput}
+         maxLength={2}
+         keyboardType="number-pad" 
+         autoCorrect={false} 
+         autoCapitalize="none" />
         <PrimaryButton >Start</PrimaryButton>
         <PrimaryButton >Reset</PrimaryButton>
       </View>
@@ -13,3 +17,29 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop:100,
+        padding:16,
+        backgroundColor:"#72063c",
+        borderRadius:10,
+        marginHorizontal:24,
+        elevation:4,
+        shadowColor:"black",
+        shadowOffset:{width:0, height:2},
+        shadowRadius:6,
+        shadowOpacity:0.25,
+    },
+    numberInput:{
+       height:50,
+       fontSize:32,
+       borderBottomColor:'#ddb52f',
+       borderBottomWidth:2,
+       color:'#ddb52f',
+       marginVertical:8,
+       fontWeight:'bold',
+       width:50,
+       textAlign:'center',
+    }
+})
